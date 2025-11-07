eac6ab52c9670f430c133081b81f62aafa73a3a7
YaelIzhak <yaelizhak2001@gmail.com>
2025-11-07 10:59:33 +0200

    Update edge_case_exercise.py
    12

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -16,8 +16,9 @@ def move(my_list, direction=None):
     return new_list
 
 def approximate_pi(n_terms):
-    total = 0.0              
-    for n in range(n_terms):
-        term = ((-1.0)**n) / (2*n + 1))   
-        total += term
-    return 4.0 * total  
+  libneiz_list = []
+  for n in range(1,11,):
+    libneiz_list.append((-1)**n/(2*n+1))
+    y = sum(libneiz_list)
+    x = y*4
+    return(x)
