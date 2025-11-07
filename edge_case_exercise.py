e89cfb7a15ef46aeb08378f0cb1fd31261b69716
YaelIzhak <yaelizhak2001@gmail.com>
2025-11-07 09:49:07 +0200

    Update edge_case_exercise.py
    10

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -18,6 +18,6 @@ def move(my_list, direction=None):
 def approximate_pi(n_terms):
     total = 0.0              
     for n in range(n_terms):
-        term = (-1.0)**n / (2*n + 1)  
+        term = (-1.0)**n / (2*n + 1)   
         total += term
-    return 4.0 * total     
+    return 4.0 * total      
