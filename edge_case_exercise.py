9ad09db62df485d6b5573ff7afd7511be2ae528f
YaelIzhak <yaelizhak2001@gmail.com>
2025-11-07 09:53:32 +0200

    Update edge_case_exercise.py
    10

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,23 +1,23 @@
 def move(my_list, direction=None):
+    if not my_list or 1 not in my_list:
+        return my_list.copy()  
+        
     index_of_one = my_list.index(1)
     new_list = my_list.copy()
-    if index_of_one == (len(my_list)-1) and direction == "right":
-        return new_list
-    elif index_of_one == 0 and direction == "left":
-        return new_list
-
-    if direction == 'right':
-        new_list[index_of_one] = 0
-        new_list[index_of_one + 1] = 1
-    elif direction == 'left':
+    
+    if direction == "left" and index_of_one > 0:
         new_list[index_of_one] = 0
         new_list[index_of_one - 1] = 1
+    elif direction == "right" and index_of_one < len(my_list) - 1:
+        new_list[index_of_one] = 0
+        new_list[index_of_one + 1] = 1
     
     return new_list
 
+
 def approximate_pi(n_terms):
-    total = 0.0              
+    total = 0
     for n in range(n_terms):
-        term = (-1.0)**n / (2*n + 1)   
-        total += term
-    return 4.0 * total      
+        total += (-1)**n / (2*n + 1)
+    
+    return round(4 * total, 4)
